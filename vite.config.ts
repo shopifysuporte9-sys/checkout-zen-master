204eb8a99dc8956ee4ded7656824c9a4ab3c004b
shopifysuporte9-sys <shopifysuporte9@gmail.com>
2025-09-20 21:29:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/checkout-zen-master/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
